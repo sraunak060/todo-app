0271f8764cfce8ddec3581516d4a90b08e6ba731
RAUNAK SINGH <raunaksingh@RAUNAKs-MacBook-Air.local>
2021-08-10 15:50:07 +0530

    first commit

---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://sraunak060:LCD2DqA6cIBX2G1L@cluster0.n1jbz.mongodb.net/myFirstDatabase?retryWrites=true&w=majoritymongodb://sraunak060:LCD2DqA6cIBX2G1L@cluster0.n1jbz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
